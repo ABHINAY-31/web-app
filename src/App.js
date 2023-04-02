2b8ffd8ad2e9d3267459dd35640ac75266770452
ABHINAY-31 <rsingh9766@gmail.com>
2023-04-02 11:09:26 +0530

    Adding Navigation and card View

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-
+// when we do an npm start this particular section will resolve to what we view over the web
+// it's a complete site
+let name="abhinay";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // return only one not more than one
+    // jsx fragment
+    <> 
+      <div className="my_navigation">
+        <nav className="navbar bg-body-tertiary">
+            <div className="container-fluid">
+              {/* <img src="" alt="" /> */}
+              <a className="navbar-brand active" href="#">Home</a>
+              <a className="navbar-brand" href="#">Blood Banks</a>
+              <a className="navbar-brand" href="#">Availability</a>
+              <a className="navbar-brand" href="#">Donation Camps</a>
+            </div>
+        </nav>
+        <div className="card">
+            <div className="card-val-1"></div>
+            <div className="card-val-2"></div>
+            <div className="card-val-3"></div>
+        </div>
+      </div>
+    </>
+      // <h1 className="my_name">Hello everyone</h1>
   );
 }
 
